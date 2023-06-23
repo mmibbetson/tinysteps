1e269990b1a1ddb02beb1a5d92cad3592d3a8866
mmibbetson <mmibbetson@gmail.com>
2023-06-23 17:33:22 +0200

    Began fleshing out user creation functions
    there were some issues with URI param parsing last night
    but as of now it is working fine.

--- src/user-validation.ts
+++ src/user-validation.ts
@@ -1,16 +1,36 @@
 // Implement functions for validating username and password
 
+import { db } from "./index.js";
+
 export function validateUsername(username: string): {
   value: string | null;
   valid: boolean;
-} {}
+} {
+  return { value: null, valid: false };
+}
 
 export function validatePassword(password: string): {
   value: string | null;
   valid: boolean;
-} {}
+} {
+  return { value: null, valid: false };
+}
 
 // check if username exists in db, if so return true, else return false
 export async function checkUserIsTaken(username: string): Promise<boolean> {
-  db.get("SELECT * FROM user WHERE username = ?", username, (err, row) => {}
+  db.get("SELECT * FROM user WHERE username = ?", username, (err, row) => {
+    if (err) {
+      throw new Error("Query failure when checking if username is taken");
+    }
+
+    if (row) {
+      if (row === undefined) {
+        return false;
+      }
+
+      return true;
+    }
+  });
+
+  throw new Error("Query failure when checking if username is taken");
 }
